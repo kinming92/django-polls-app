cbf2dea7b1fb7aea5d9a1d4081a87255e44a279b
Kevin <huih1026@gmail.com>
2021-09-30 22:12:10 -0700

    first commit today

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "/static"
 
 
 MEDIA_URL = '/files/'
